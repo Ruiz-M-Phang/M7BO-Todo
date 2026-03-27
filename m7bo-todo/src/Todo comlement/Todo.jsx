5eba1ad42954c309a3b21c0bce20d6613e50cfae
lucas <38404@ma-web.nl>
2026-03-27 14:20:10 +0100

    Add files via upload

---
+++ m7bo-todo/src/Todo comlement/Todo.jsx
@@ -0,0 +1,12 @@
+
+
+function Todo() {
+  return (
+    <div>
+      <h1>Todo list</h1>
+      <h2>Add a new todo</h2>
+        <input type="text" placeholder="Enter a new todo" />
+        <button>Add</button>
+     </div>
+  );
+}

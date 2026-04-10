a90f72735a2eac6ffce437a1b175cb962a010e53
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2026-04-10 15:01:21 +0200

    Nav bar

--- vite-project/src/Navigatie/Navbar.jsx
+++ vite-project/src/Navigatie/Navbar.jsx
@@ -5,12 +5,14 @@ export function Navbar() {
   return (
     <div>
         {/* hier komt de code navbar */}
-        <nav className={styles.navbar}>
-            <ul className={styles.navbar__links}>
-                <li>past due</li>
-                <li>completed</li>
-            </ul>
-        </nav>
+        <div className={styles.background}>
+          <nav className={styles.navbar}>
+              <ul className={styles.navbar__list}>
+                  <li className={styles.list__item}><button className={styles.list__button}><a href="" className={styles.item__link}>past due</a></button></li>
+                  <li className={styles.list__item}><button className={styles.list__button}><a href="" className={styles.item__link}>completed</a></button></li>
+              </ul>
+          </nav>
+        </div>
     </div>
   )
 }
